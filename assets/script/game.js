26da337e15a82e4a220a2caccfdd69ccb2f3cb9b
bairdko <bairdko@gmail.com>
2018-06-28 18:17:48 -0500

    mostly done, weird timing issues that need to be fixed

--- assets/script/game.js
+++ assets/script/game.js
@@ -1,13 +1,14 @@
 
 //generate the target number for the game
 
-var targetNum = $("#targetNum");
+var targetLoc = $("#targetNum");
+var targetNum = 0;
 
 function setTarget(){
 
-  //make sure target is at least 10
-  var num = Math.floor(Math.random() * 40) + 10;
-  targetNum.text(num);
+  //make sure target is at least generate number between 19 & 120
+  targetNum = Math.floor(Math.random() * 101) + 19;
+  targetLoc.text(targetNum);
 }
 
 setTarget();
@@ -19,39 +20,67 @@ setTarget();
 function assignVal(cryID){
 
   var crystalsVal = $(cryID);
-  var num = Math.floor(Math.random() * 9) + 1;
+  var num = Math.floor(Math.random() * 11) + 1;
   crystalsVal.attr("val",num);
 
 }
 
 //assign value to each crystal
 //maybe throw numbers into an array to condense this
+
 assignVal("#sapphire");
 assignVal("#ruby");
 assignVal("#emerald");
-assignVal("#diamond");
+assignVal("#amethyst");
 
-//make a function that takes the values of clicked crystals and adds them
-// num =  crystal.attr("val") this just pulls the value
-// += that is to concatenate 
-
-//if statement 
-// if your added crystal values goes over the targetvalue then you lose
-// else you can keep playing
-// call your adding function within the else bit
-
-if (num > targetNum){
-  //you lose restart game
-}
-else if (num === targetNum){
-  //you win restart game
-}
-else{
-  //keep playing
-  //adding function
-}
+var playerScore = 0; 
+var wins = 0;
+var losses = 0;
 
 //restart game 
 // call settarget again
 // also call assignVal again
 // add win or loss score 
+
+function restartGame(){
+  setTarget();
+
+  assignVal("#sapphire");
+  assignVal("#ruby");
+  assignVal("#emerald");
+  assignVal("#amethyst");
+
+
+}
+
+var addCrystals = $('.crystals').on('click',function(){
+  //if statement 
+  // if your added crystal values goes over the targetvalue then you lose
+  // else you can keep playing
+
+
+  if (playerScore > targetNum){
+    alert("You lose!");
+    wins++;
+    $('#wins').text(wins);
+    setTimeout(restartGame,500);
+
+  }
+  else if (playerScore === targetNum){
+    alert("You win!");
+    losses++;
+    $('#losses').text(losses);
+    setTimeout(restartGame,500);
+  }
+  else{
+    // add player scores
+    var crystalVal = this.getAttribute('val');
+
+    playerScore += parseInt(crystalVal);
+
+    $('#scoreBox').text(playerScore);
+  }
+
+});
+
+
